f0e2b844a8df7d8cf55e2c143a7496d85e287a5d
julio delgado <jjdelgadoabreu@student.fullsail.edu>
2022-01-15 22:59:16 -0500

    add frame_info class

---
+++ VulkanEngine/hpp/ve_frame_info.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "ve_camera.hpp"
+
+// lib
+#include <vulkan/vulkan.h>
+
+namespace ve {
+
+	struct frame_info {
+		int frameIndex;
+		float frameTime;
+		VkCommandBuffer commandBuffer;
+		ve_camera& camera;
+	};
+
+} // namespace ve
